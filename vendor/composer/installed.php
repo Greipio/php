1d690beb818510ba16a8cc03eb08c0a8a758dc65
GRE Development Ltd <info@gredev.io>
2022-01-29 17:53:06 +0300

    -

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f7629a6ce4d25b292393235b423833d6a169e9ee',
+    'name' => 'gre/geoip',
+  ),
+  'versions' => 
+  array (
+    'gre/geoip' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f7629a6ce4d25b292393235b423833d6a169e9ee',
+    ),
+  ),
+);
