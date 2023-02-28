8ce46798bed81cd24608f420d8b9d895bc5277fc
GRE Development Ltd <info@gredev.io>
2023-02-28 04:36:30 +0300

    Major changes to the library.

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 <div align="center">
-    <h1>GRE GeoIP for PHP</h1>
-    <p>The official PHP library for GRE GeoIP API</p>
+    <h1>Greip API</h1>
+    <p>The official PHP library for Greip.io</p>
     <br />
-    <a href="https://github.com/gre-dev/GeoIP-PHP/issues/new">Report Issue</a> · 
-    <a href="https://github.com/gre-dev/GeoIP-PHP/discussions/new">Request Feature</a> · 
-    <a href="https://www.gredev.io/en/GeoIP" target="_BLANK">API Home Page</a> · 
-    <a href="https://geoip-docs.gredev.io/sdks/php" target="_BLANK">API Docs</a>
+    <a href="https://github.com/Greipio/Greip-PHP/issues/new">Report Issue</a> · 
+    <a href="https://github.com/Greipio/Greip-PHP/discussions">Request Feature</a> · 
+    <a href="https://greip.io/" target="_BLANK">API Home Page</a> · 
+    <a href="https://docs.greip.io/" target="_BLANK">API Docs</a>
     <br />
     <br />
     <a href="https://packagist.org/packages/gre/geoip" title="Packagist" href="_BLANK"><img src="https://img.shields.io/badge/packagist-CB3837?style=for-the-badge&logo=packagist&logoColor=white&color=f28d1a"></a>
     <img src="https://img.shields.io/badge/php-CB3837?style=for-the-badge&logo=php&logoColor=white&color=4F5B93" title="Javascript">
-    <a href="https://github.com/gre-dev/GeoIP-PHP" title="Github Repo" href="_BLANK"><img src="https://img.shields.io/badge/GitHub-CB3837?style=for-the-badge&logo=github&logoColor=white&color=black"></a>
+    <a href="https://github.com/Greipio/Greip-PHP" title="Github Repo" href="_BLANK"><img src="https://img.shields.io/badge/GitHub-CB3837?style=for-the-badge&logo=github&logoColor=white&color=black"></a>
     <a href="https://www.patreon.com/gredev" title="Patreon Profile - GRE Development Ltd." href="_BLANK"><img src="https://img.shields.io/badge/Patreon-ff424e?style=for-the-badge&logo=patreon&logoColor=white"></a>
 </div>
 <br />
@@ -20,7 +20,7 @@
 
 ![Packagist Version](https://img.shields.io/packagist/v/gre/geoip?color=brightgreen&label=Stable&logo=packagist&logoColor=white)
 &nbsp;&nbsp;
-![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/gre-dev/GeoIP-PHP?color=brightgreen&label=Size&logo=packagist&logoColor=white)
+![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/Greipio/Greip-PHP?color=brightgreen&label=Size&logo=packagist&logoColor=white)
 &nbsp;&nbsp;
 ![API Status](https://img.shields.io/website?down_color=orange&down_message=down&label=API%20status&up_color=brightgreen&up_message=up&url=https%3A%2F%2Fgregeoip.com)
 &nbsp;&nbsp;
@@ -37,38 +37,163 @@
 ```
 composer require gre/geoip
 ```
-<br /><br />
+<br />
 
 # Usage
-Let's say that we want to get the information of a specific IP Address. So we do the following:
+Here's how you use the API Methods:
 <br /><br />
 
+## 1. IP Geolocation
 ```php
 include_once './vendor/autoload.php';
 
-// Using the library
-use GRE\GeoIP\GeoIP;
+// Declaring the classes we need
+$Config = new Greip\Config();
+$GeoIP = new Greip\GeoIP();
 
-// Declaring the library's class
-$GREGeoIP = new GeoIP();
 // Setting the API Key
-$GREGeoIP->setKey('<API-Key>');
+$Config->setKey('<API-Key>');
 
 // Sending the request and storing the output in a variable
-$GeoIP_Response = $GREGeoIP->lookup('1.1.1.1');
-// Printing the reponse
+$GeoIP_Response = $GeoIP->lookup('1.1.1.1');
+// Printing the response
 print_r($GeoIP_Response);
 ```
-<br /><br />
+<br />
+
+## 2. ASN Lookup
+```php
+include_once './vendor/autoload.php';
+
+// Declaring the classes we need
+$Config = new Greip\Config();
+$GeoIP = new Greip\GeoIP();
+
+// Setting the API Key
+$Config->setKey('<API-Key>');
+
+// Sending the request and storing the output in a variable
+$ASN_Response = $GeoIP->asn('AS01');
+// Printing the response
+print_r($ASN_Response);
+```
+<br />
+
+## 3. Country Lookup
+```php
+include_once './vendor/autoload.php';
+
+// Declaring the classes we need
+$Config = new Greip\Config();
+$GeoIP = new Greip\GeoIP();
+
+// Setting the API Key
+$Config->setKey('<API-Key>');
+
+// Sending the request and storing the output in a variable
+$Country_Response = $GeoIP->country('US', ['language', 'timezone', 'currency']);
+// Printing the response
+print_r($Country_Response);
+```
+<br />
+
+## 4. Email Validation
+```php
+include_once './vendor/autoload.php';
+
+// Declaring the classes we need
+$Config = new Greip\Config();
+$Fraud = new Greip\Fraud();
+
+// Setting the API Key
+$Config->setKey('<API-Key>');
+
+// Sending the request and storing the output in a variable
+$Email_Response = $Fraud->email('example@domain.com');
+// Printing the response
+print_r($Email_Response);
+```
+<br />
+
+## 5. Phone Validation
+```php
+include_once './vendor/autoload.php';
+
+// Declaring the classes we need
+$Config = new Greip\Config();
+$Fraud = new Greip\Fraud();
+
+// Setting the API Key
+$Config->setKey('<API-Key>');
+
+// Sending the request and storing the output in a variable
+$Phone_Response = $Fraud->phone('000000000', 'US');
+// Printing the response
+print_r($Phone_Response);
+```
+<br />
+
+## 6. Profanity Detection
+```php
+include_once './vendor/autoload.php';
+
+// Declaring the classes we need
+$Config = new Greip\Config();
+$Fraud = new Greip\Fraud();
+
+// Setting the API Key
+$Config->setKey('<API-Key>');
+
+// Sending the request and storing the output in a variable
+$Profanity_Response = $Fraud->profanity('This is a sample text', true, false);
+// Printing the response
+print_r($Profanity_Response);
+```
+<br />
+
+## 7. Payment Fraud Prevention
+```php
+include_once './vendor/autoload.php';
+
+// Declaring the classes we need
+$Config = new Greip\Config();
+$Fraud = new Greip\Fraud();
+
+// Setting the API Key
+$Config->setKey('<API-Key>');
+
+// Declaring Transaction Data
+$data = [
+    'action' => 'purchase',
+    'website_domain' => 'example.com',
+    'merchant_id' => 21,
+    'customer_id' => 1,
+    'customer_ip' => '1.0.0.2',
+    'customer_email' => 'asdfasdf@gmasail.com',
+    'customer_phone' => '32423434',
+    'customer_country' => 'US',
+    'transaction_amount' => 100000,
+    'transaction_currency' => 'USD',
+    'customer_useragent' => 'Mozill almaden sdfwer',
+];
+
+// Sending the request and storing the output in a variable
+$Payment_Response = $Fraud->payment($data);
+// Printing the response
+print_r($Payment_Response);
+```
+
+<br />
 
 # Options, Methods and More
-You can find the full guide of this package by visiting our [Documentation Page](https://geoip-docs.gredev.io/sdks/php).
+You can find the full guide of this package by visiting our [Documentation Page](https://docs.greip.io/).
 
-<br /><br />
+<br />
+
 # Credits
-* [GRE Development Ltd.](https://www.gredev.io/en/)
-* [All Contributors](https://github.com/gre-dev/GeoIP-PHP/graphs/contributors)
+* [Greip Team](https://greip.io/)
+* [All Contributors](https://github.com/Greipio/Greip-PHP/graphs/contributors)
 <br /><br />
 
 # License
-The MIT License (MIT). Please see [License](https://github.com/gre-dev/GeoIP-PHP/blob/main/LICENSE) File for more information.
+The MIT License (MIT). Please read the [License](https://github.com/Greipio/Greip-PHP/blob/master/LICENSE) file for more information.
--- src/GeoIP.php
+++ src/GeoIP.php
@@ -1,36 +1,12 @@
 <?php
-namespace GRE\GeoIP;
+
+namespace Greip;
+
 use Exception;
 
-/**
- * GeoIP Class
- *
- * This class will help you use the functionalities of the GeoIP API
- * For more information please check out https://geoip-docs.gredev.io
- * 
- * @package gre\geoip
- * @version 1.1.9
- * @author GRE Development Ltd. <info@gredev.io>
- * @copyright 2016-2022 GRE Development Ltd.
- * @license MIT
- */
-
-class GeoIP extends Exception
+class Config
 {
-
-   /**
-    * @var string $APIKey This is your API Key.
-    * @var string $APIEndpoint The GRE GeoIP endpoint.
-    * @var array $AvailableGeoIPParams List of the params available to use in both GeoIP & Lookup modules of the API.
-    * @var array $AvailableCountryParams List of the params available to use in the Country module of the API.
-    * @var string $isError Can be used AFTER MAKING A REQUEST to determine if the API returned an error.
-    */
-   private $APIKey = '';
-   private $APIEndpoint = 'https://gregeoip.com/';
-   private $AvailableGeoIPParams = ['location', 'security', 'timezone', 'currency', 'device'];
-   private $AvailableCountryParams = ['language', 'flag', 'currency', 'timezone'];
-   private $AvailableLanguages = ['EN', 'AR', 'DE', 'FR', 'ES', 'JA', 'ZH', 'RU'];
-   public $isError = false;
+   public static $APIKey = '';
 
    /**
     * setKey method 
@@ -42,25 +18,61 @@ class GeoIP extends Exception
    public function setKey($key): bool
    {
       if (!empty($key)) {
-         $this->APIKey = $key;
+         self::$APIKey = $key;
          return true;
       } else {
          return false;
       }
    }
 
+   public function getKey(): string
+   {
+      return self::$APIKey;
+   }
+}
+
+/**
+ * GeoIP Class
+ *
+ * This class will help you use the functionalities of Greip IP Geolocation & Country API
+ * For more information please check out https://docs.greip.io
+ * 
+ * @package gre\geoip
+ * @version 2.0
+ * @author Greip <info@greip.io>
+ * @copyright 2016-2023 Greip
+ * @license MIT
+ */
+
+class GeoIP extends Exception
+{
    /**
-    * lookup method 
+    * @var string $APIEndpoint The GRE GeoIP endpoint.
+    * @var array $AvailableGeoIPParams List of the params available to use in both GeoIP & Lookup modules of the API.
+    * @var array $AvailableCountryParams List of the params available to use in the Country module of the API.
+    * @var string $isError Can be used AFTER MAKING A REQUEST to determine if the API returned an error.
+    */
+   private $APIEndpoint = 'https://gregeoip.com/';
+   private $AvailableGeoIPParams = ['location', 'security', 'timezone', 'currency', 'device'];
+   private $AvailableCountryParams = ['language', 'flag', 'currency', 'timezone'];
+   private $AvailableLanguages = ['EN', 'AR', 'DE', 'FR', 'ES', 'JA', 'ZH', 'RU'];
+   public $isError = false;
+
+   /**
+    * IP Geolocation Method 
     *
     * @param string $ip The IP Address you want to lookup
     * @param array $params You can pass the modules you want to fetch for that $ip. This array accepts `location`, `security`, `timezone`, `currency` and/or `device`.
     * @param string $lang Sets the output language. It can be `EN`, `AR`, `DE`, `FR`, `ES`, `JA`, `ZH` or `RU`.
-    * @param string $mode You pass `test` to this variable, so your account plan will not be affected while integrating the library and the API will return fake information for this case. You can set it to `live` again to back to the `production` mode. @see https://geoip-docs.gredev.io/sdks/php/country-method#options
+    * @param string $mode You pass `test` to this variable, so your account plan will not be affected while integrating the library and the API will return fake information for this case. You can set it to `live` again to back to the `production` mode.
+    * @see https://docs.greip.io/methods/lookup-ip-address
     *
     * @return array The $ip information
     */
    public function lookup($ip, $params = [], $lang = 'EN', $mode = 'live'): array
    {
+      $configClass = new Config();
+
       if (!empty($ip)) {
          $tempParams = '';
          foreach ($params as $perParam) {
@@ -75,18 +87,18 @@ class GeoIP extends Exception
          }
          if (!empty($tempParams)) $tempParams = substr($tempParams, 0, -1);
 
-         if (!in_array($lang, $this->AvailableLanguages)){
+         if (!in_array($lang, $this->AvailableLanguages)) {
             $this->isError = true;
-            throw new Exception('The language you specified ('.$lang.') is unknown.');
+            throw new Exception('The language you specified (' . $lang . ') is unknown.');
          }
 
-         if ($mode !== 'live' && $mode !== 'test'){
+         if ($mode !== 'live' && $mode !== 'test') {
             $this->isError = true;
-            throw new Exception('The mode you specified ('.$mode.') is unknown. You should use `live` or `test`.');
+            throw new Exception('The mode you specified (' . $mode . ') is unknown. You should use `live` or `test`.');
          }
 
          $localParams = array(
-            'key' => $this->APIKey,
+            'key' => $configClass->getKey(),
             'ip' => $ip,
             'lang' => $lang,
             'params' => $tempParams,
@@ -112,17 +124,20 @@ class GeoIP extends Exception
    }
 
    /**
-    * country method 
+    * Country Lookup Method 
     *
     * @param string $countryCode The ISO 3166-1 alpha-2 code format of the country. [Learn More](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2)
     * @param array $params You can pass the modules you want to fetch for that $ip. This array accepts `location`, `security`, `timezone`, `currency` and/or `device`.
     * @param string $lang Sets the output language. It can be `EN`, `AR`, `DE`, `FR`, `ES`, `JA`, `ZH` or `RU`.
-    * @param string $mode You pass `test` to this variable, so your account plan will not be affected while integrating the library and the API will return fake information for this case. You can set it to `live` again to back to the `production` mode. @see https://geoip-docs.gredev.io/sdks/php/country-method#options
+    * @param string $mode You pass `test` to this variable, so your account plan will not be affected while integrating the library and the API will return fake information for this case. You can set it to `live` again to back to the `production` mode.
+    * @see https://docs.greip.io/methods/country-data-api
     *
     * @return array The country information
     */
    public function country($countryCode, $params = [], $lang = 'EN', $mode = 'live'): array
    {
+      $configClass = new Config();
+
       if (!empty($countryCode)) {
          $tempParams = '';
          foreach ($params as $perParam) {
@@ -137,18 +152,18 @@ class GeoIP extends Exception
          }
          if (!empty($tempParams)) $tempParams = substr($tempParams, 0, -1);
 
-         if (!in_array($lang, $this->AvailableLanguages)){
+         if (!in_array($lang, $this->AvailableLanguages)) {
             $this->isError = true;
-            throw new Exception('The language you specified ('.$lang.') is unknown.');
+            throw new Exception('The language you specified (' . $lang . ') is unknown.');
          }
 
-         if ($mode !== 'live' && $mode !== 'test'){
+         if ($mode !== 'live' && $mode !== 'test') {
             $this->isError = true;
-            throw new Exception('The mode you specified ('.$mode.') is unknown. You should use `live` or `test`.');
+            throw new Exception('The mode you specified (' . $mode . ') is unknown. You should use `live` or `test`.');
          }
 
          $localParams = array(
-            'key' => $this->APIKey,
+            'key' => $configClass->getKey(),
             'CountryCode' => $countryCode,
             'params' => $tempParams,
             'lang' => $lang,
@@ -170,6 +185,263 @@ class GeoIP extends Exception
       } else {
          $this->isError = true;
          throw new Exception('The $countryCode parameter is required. You passed an empty value.');
+      }
+   }
+
+   /**
+    * ASN Lookup Method 
+    *
+    * @param string $asn The AS Number you want to lookup.
+    * @param boolean $isList Set this to `true` if you want to the API to return all IPv4 & IPv6 routes.
+    * @param string $mode You pass `test` to this variable, so your account plan will not be affected while integrating the library and the API will return fake information for this case. You can set it to `live` again to back to the `production` mode.
+    * @see https://docs.greip.io/methods/asn-lookup-api
+    *
+    * @return array The ASN Data
+    */
+   public function asn($asn, $isList = false, $mode = 'live'): array
+   {
+      $configClass = new Config();
+
+      if (!empty($asn)) {
+         if (!is_bool($isList)) $isList = false;
+
+         if ($mode !== 'live' && $mode !== 'test') {
+            $this->isError = true;
+            throw new Exception('The mode you specified (' . $mode . ') is unknown. You should use `live` or `test`.');
+         }
+
+         $localParams = array(
+            'key' => $configClass->getKey(),
+            'asn' => $asn,
+            'isList' => ($isList) ? 'yes' : 'no',
+            'mode' => $mode,
+            'source' => 'PHP-SDK'
+         );
+         $ch = curl_init();
+         curl_setopt($ch, CURLOPT_URL, $this->APIEndpoint . 'ASNLookup?' . http_build_query($localParams));
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+         $APIResponse = curl_exec($ch);
+         curl_close($ch);
+         $decodedResponse = json_decode($APIResponse, true);
+
+         if (is_array($decodedResponse) && in_array('status', array_keys($decodedResponse)) && $decodedResponse['status'] !== 'success') {
+            $this->isError = true;
+         }
+
+         return $decodedResponse;
+      } else {
+         $this->isError = true;
+         throw new Exception('The $asn parameter is required. You passed an empty value.');
+      }
+   }
+}
+
+/**
+ * Fraud Class
+ *
+ * This class will help you use the functionalities of Greip Fraud Prevention
+ * For more information please check out https://docs.greip.io
+ * 
+ * @package gre\geoip
+ * @version 2.0
+ * @author Greip <info@greip.io>
+ * @copyright 2016-2023 Greip
+ * @license MIT
+ */
+
+class Fraud extends Exception
+{
+   /**
+    * @var string $APIEndpoint The GRE GeoIP endpoint.
+    * @var array $AvailableGeoIPParams List of the params available to use in both GeoIP & Lookup modules of the API.
+    * @var array $AvailableCountryParams List of the params available to use in the Country module of the API.
+    * @var string $isError Can be used AFTER MAKING A REQUEST to determine if the API returned an error.
+    */
+   private $APIEndpoint = 'https://gregeoip.com/';
+   private $AvailableCountryParams = ['language', 'flag', 'currency', 'timezone'];
+   private $AvailableLanguages = ['EN', 'AR', 'DE', 'FR', 'ES', 'JA', 'ZH', 'RU'];
+   public $isError = false;
+
+   /**
+    * Email Validation Method 
+    *
+    * @param string $email The email addresss you want to validate.
+    * @param string $mode You pass `test` to this variable, so your account plan will not be affected while integrating the library and the API will return fake information for this case. You can set it to `live` again to back to the `production` mode.
+    * @see https://docs.greip.io/methods/data-validation/email-validation
+    *
+    * @return array
+    */
+   public function email($email, $mode = 'live'): array|null
+   {
+      $configClass = new Config();
+
+      if (!empty($email)) {
+         if ($mode !== 'live' && $mode !== 'test') {
+            $this->isError = true;
+            throw new Exception('The mode you specified (' . $mode . ') is unknown. You should use `live` or `test`.');
+         }
+
+         $localParams = array(
+            'key' => $configClass->getKey(),
+            'email' => $email,
+            'mode' => $mode,
+            'source' => 'PHP-SDK'
+         );
+         $ch = curl_init();
+         curl_setopt($ch, CURLOPT_URL, $this->APIEndpoint . 'validateEmail?' . http_build_query($localParams));
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+         $APIResponse = curl_exec($ch);
+         curl_close($ch);
+         $decodedResponse = json_decode($APIResponse, true);
+
+         if (is_array($decodedResponse) && in_array('status', array_keys($decodedResponse)) && $decodedResponse['status'] !== 'success') {
+            $this->isError = true;
+         }
+
+         return $decodedResponse;
+      } else {
+         $this->isError = true;
+         throw new Exception('The $phone parameter is required. You passed an empty value.');
+      }
+   }
+
+   /**
+    * Phone Validation Method 
+    *
+    * @param string $phone The phone number you want to validate.
+    * @param string $countryCode The ISO 3166-1 alpha-2 code format of the user. [Learn More](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2)
+    * @param string $mode You pass `test` to this variable, so your account plan will not be affected while integrating the library and the API will return fake information for this case. You can set it to `live` again to back to the `production` mode.
+    * @see https://docs.greip.io/methods/data-validation/phone-number-validation
+    *
+    * @return array
+    */
+   public function phone($phone, $countryCode, $mode = 'live'): array|null
+   {
+      $configClass = new Config();
+
+      if (!empty($phone) && !empty($countryCode)) {
+         if ($mode !== 'live' && $mode !== 'test') {
+            $this->isError = true;
+            throw new Exception('The mode you specified (' . $mode . ') is unknown. You should use `live` or `test`.');
+         }
+
+         $localParams = array(
+            'key' => $configClass->getKey(),
+            'phone' => $phone,
+            'countryCode' => $countryCode,
+            'mode' => $mode,
+            'source' => 'PHP-SDK'
+         );
+         $ch = curl_init();
+         curl_setopt($ch, CURLOPT_URL, $this->APIEndpoint . 'validatePhone?' . http_build_query($localParams));
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+         $APIResponse = curl_exec($ch);
+         curl_close($ch);
+         $decodedResponse = json_decode($APIResponse, true);
+
+         if (is_array($decodedResponse) && in_array('status', array_keys($decodedResponse)) && $decodedResponse['status'] !== 'success') {
+            $this->isError = true;
+         }
+
+         return $decodedResponse;
+      } else {
+         $this->isError = true;
+         throw new Exception('The $phone/$countryCode parameter is required. You passed an empty value.');
+      }
+   }
+
+   /**
+    * Profanity Detection Method 
+    *
+    * @param string $text The text you want to check for profanity.
+    * @param string $listBadWords Used to list the detected words and prases as an Array.
+    * @param string $scoreOnly Returns only the score of the text and whether it's safe or not.
+    * @param string $mode You pass `test` to this variable, so your account plan will not be affected while integrating the library and the API will return fake information for this case. You can set it to `live` again to back to the `production` mode.
+    * @see https://docs.greip.io/methods/profanity-detection-api
+    *
+    * @return array
+    */
+   public function profanity($text, $listBadWords = false, $scoreOnly = false, $mode = 'live'): array|null
+   {
+      $configClass = new Config();
+
+      if (!empty($text)) {
+         if ($mode !== 'live' && $mode !== 'test') {
+            $this->isError = true;
+            throw new Exception('The mode you specified (' . $mode . ') is unknown. You should use `live` or `test`.');
+         }
+
+         if (!is_bool($listBadWords)) $listBadWords = false;
+         if (!is_bool($scoreOnly)) $scoreOnly = false;
+
+         $localParams = array(
+            'key' => $configClass->getKey(),
+            'text' => $text,
+            'listBadWords' => ($listBadWords) ? 'yes' : 'no',
+            'scoreOnly' => ($scoreOnly) ? 'yes' : 'no',
+            'mode' => $mode,
+            'source' => 'PHP-SDK'
+         );
+         $ch = curl_init();
+         curl_setopt($ch, CURLOPT_URL, $this->APIEndpoint . 'badWords?' . http_build_query($localParams));
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+         $APIResponse = curl_exec($ch);
+         curl_close($ch);
+         $decodedResponse = json_decode($APIResponse, true);
+
+         if (is_array($decodedResponse) && in_array('status', array_keys($decodedResponse)) && $decodedResponse['status'] !== 'success') {
+            $this->isError = true;
+         }
+
+         return $decodedResponse;
+      } else {
+         $this->isError = true;
+         throw new Exception('The $text parameter is required. You passed an empty value.');
+      }
+   }
+
+   /**
+    * Payment Fraud Prevention Method 
+    *
+    * @param array $data The infomation of the transaction.
+    * @param string $mode You pass `test` to this variable, so your account plan will not be affected while integrating the library and the API will return fake information for this case. You can set it to `live` again to back to the `production` mode.
+    * @see https://docs.greip.io/methods/payment-fraud-prevention
+    *
+    * @return array
+    */
+   public function payment($data, $mode = 'live'): array|null
+   {
+      $configClass = new Config();
+
+      if (!empty($data)) {
+         if ($mode !== 'live' && $mode !== 'test') {
+            $this->isError = true;
+            throw new Exception('The mode you specified (' . $mode . ') is unknown. You should use `live` or `test`.');
+         }
+
+         $localParams = array(
+            'key' => $configClass->getKey(),
+            'mode' => $mode,
+            'source' => 'PHP-SDK',
+            'data' => $data,
+         );
+         $ch = curl_init();
+         curl_setopt($ch, CURLOPT_URL, $this->APIEndpoint . 'paymentFraud');
+         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($localParams, true));
+         $APIResponse = curl_exec($ch);
+         curl_close($ch);
+         $decodedResponse = json_decode($APIResponse, true);
+
+         if (is_array($decodedResponse) && in_array('status', array_keys($decodedResponse)) && $decodedResponse['status'] !== 'success') {
+            $this->isError = true;
+         }
+
+         return $decodedResponse;
+      } else {
+         $this->isError = true;
+         throw new Exception('The $data parameter is required. You passed an empty value.');
       }
    }
 }
